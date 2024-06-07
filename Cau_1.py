43f8555fec2dff15253508e063ab40f965ee36e9
Huỳnh Tuấn Minh <20521598@gm.uit.edu.vn>
2024-06-07 08:58:43 +0700

    Update Cau_1.py

--- Cau_1.py
+++ Cau_1.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("This i a next level SEO app")
 st.write("Make your...")
 st.write("What is your...")
-st.text_input(label='',value='',label_visibility='hiden')
+st.text_input(label='',value='',label_visibility='hidden')
